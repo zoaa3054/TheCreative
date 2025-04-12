ee7a033f38076b16cca8f20b2984aedb61ad29ac
zoaa3054 <zoaa3054@gmail.com>
2025-04-12 21:52:23 +0200

    .

--- TheCreativeBackend/db.js
+++ TheCreativeBackend/db.js
@@ -4,8 +4,7 @@ let dbConnection;
 
 module.exports = {
     connectToDb : (cb)=>{
-        const uri = "mongodb+srv://atlas-sample-dataset-load-67fabeda3322325ac06915ec:n3qS2MC6FA6Ep4lG@cluster0.5plqdk3.mongodb.net/?appName=Cluster0";
-        MongoClient.connect(uri)
+        const uri = "mongodb+srv://thecreativeinmath:n3qS2MC6FA6Ep4lG@cluster0.5plqdk3.mongodb.net/?appName=Cluster0";        MongoClient.connect(uri)
         .then((client)=>{
             dbConnection = client.db();
             return cb();
