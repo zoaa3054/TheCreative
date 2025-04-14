1bb18d54b0c5bc5c64a477dd08540d64d0dd4a6a
zoaa3054 <zoaa3054@gmail.com>
2025-04-14 18:47:05 +0200

    .

--- TheCreative/public/sw.js
+++ TheCreative/public/sw.js
@@ -1,7 +1,8 @@
-alert("khel")
-self.addEventListener('push', (e)=>{
-  console.log(e);
+self.addEventListener('push', (e) => {
+  const data = e.data?.json() || {}; // Safely get JSON data if present
+
   self.registration.showNotification("New Lecture", {
-    body: e,
-  })
-})
+    body: data.body || 'You have a new notification!',
+    icon: '/icons/icon-192x192.png', // optional
+  });
+});
