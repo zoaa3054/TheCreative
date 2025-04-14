f538969583036113f22a3f4bd86174e5e297b50b
zoaa3054 <zoaa3054@gmail.com>
2025-04-14 15:05:11 +0200

    .

--- TheCreativeBackend/messaging.js
+++ TheCreativeBackend/messaging.js
@@ -3,7 +3,7 @@ const webPush = require('web-push');
 module.exports = {
     sendNotification: async (token, notifBody) => {
 
-        webPush.setVapidDetails('mailto:easymath85@gmail.com', process.env.PUSH_PUBLIC_KEY, process.env.PUSH_PRIVATE_KEY);
+        webPush.setVapidDetails('mailto:easymath85@gmail.com', process.env.REACT_APP_PUSH_PUBLIC_KEY, process.env.PUSH_PRIVATE_KEY);
         webPush.sendNotification(token, notifBody)
         console.log('Notification sent');
     }
