9817ce65a0babb369b7d3cb370799ac9548ceafc
zoaa3054 <zoaa3054@gmail.com>
2025-04-14 15:17:30 +0200

    .

--- TheCreativeBackend/messaging.js
+++ TheCreativeBackend/messaging.js
@@ -1,8 +1,8 @@
 const webPush = require('web-push');
+require('dotenv').config();
 
 module.exports = {
     sendNotification: async (token, notifBody) => {
-
         webPush.setVapidDetails('mailto:easymath85@gmail.com', process.env.REACT_APP_PUSH_PUBLIC_KEY, process.env.PUSH_PRIVATE_KEY);
         webPush.sendNotification(token, notifBody)
         console.log('Notification sent');
