fdee2728383d49496299412e6f54026a40eae120
zoaa3054 <zoaa3054@gmail.com>
2025-04-14 18:57:25 +0200

    .

--- TheCreative/public/sw.js
+++ TheCreative/public/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', (e) => {
-  const data = e.data?.json() || {}; // Safely get JSON data if present
-
+  const data = e.data
+console.log(data);
   self.registration.showNotification("New Lecture", {
     body: data.body || 'You have a new notification!',
     icon: '/icons/icon-192x192.png', // optional
---
+++ TheCreativeBackend/test.js
@@ -0,0 +1,10 @@
+const {sendNotification} = require('./messaging.js');
+
+sendNotification({
+    endpoint: 'https://fcm.googleapis.com/fcm/send/cmIJk_dot_w:APA91bEM7Fnadn9JnR_qcF3E6Z6yCx5Mdsjahv0voub3q-GRrECTHJ1L-y-o9-sbYY5WcoD0Sqzrwg8sR-5fTnuH1djQqfO7zpEX_lHiRjMwqhSKWiGu1MA41SiRujrK4m6yKyK09KQq',
+    expirationTime: null,
+    keys: {
+      p256dh: 'BA21VvVAQiOO-yyoCetgf5dVRdOM7x516Oqxj6kPWeUNJSVUvUkPHSvwWgM6EQIF2Z11kXE6SRnJiUXKm_CtPNo',
+      auth: 'eCc70s120uttF9VaqcpDDA'
+    }
+  }, "hello");
